49917496b09c3d0551a157158684b80b47865856
ARKADEB DUTTA <r0nn13_arkadeb@outlook.com>
2022-05-17 22:14:58 +0530

    Updated Contents

--- file_upload/file_upload/Models/file.js
+++ file_upload/file_upload/Models/file.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const ud=new mongoose.Schema({
     path:{type: String,required: true},
-    caption:{type: String, required: true}
+    caption:{type: String, required: true},
+    keyword:[{type:String }]
 })
 module.exports= mongoose.model('files',ud);
--- file_upload/file_upload/Server.js
+++ file_upload/file_upload/Server.js
@@ -1,10 +1,12 @@
 var express	=	require("express");
+var bodyParser=require('body-parser');
 var multer	=	require('multer');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const save_file= require('./Models/file');
-
 var app	=	express();
+var fs = require('fs');
+
 dotenv.config();
 mongoose.connect(process.env.DB_CONNECTION, {
   useNewUrlParser: true,
@@ -23,20 +25,75 @@ var storage	=	multer.diskStorage({
 });
 var upload = multer({ storage : storage}).single('files');
 console.log(upload);
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/',function(req,res){
-      res.sendFile(__dirname + "/main.html");
+  res.sendFile(__dirname + "/home.html");
+});
+app.get('/home',function(req,res){
+  res.sendFile(__dirname + "/home.html");
+});
+app.get('/about',function(req,res){
+  res.sendFile(__dirname + "/about.html");
 });
 app.get('/upload', function(req,res){
   res.sendFile(__dirname+'/upload.html') 
-})
+});
+app.get('/search', function(req,res){
+  res.sendFile(__dirname+'/search.html')
+});
 
-app.get('/search', async function(req,res){
-  //const keyword= req.body["keyword"];
-  const keyword="qwerty"
+app.post('/search',async function(req,res){
+  /*search(req,res,async function(err) {
+		if(err) {
+      console.log(err);
+			return res.end("Error searching file.");
+		}
+    const keyword= req.body.caption;
+    console.log(keyword);
+
+    const data= await save_file.find({caption:keyword});
+    res.send(data);
+	});*/
+  //const keyword= req.body.key;
+  //res.send(keyword);
+  const keyword=req.body.key;
+  console.log(keyword);
   const data= await save_file.find({caption:keyword})
-  res.send(data);
-})
+  console.log(data[0].path);
+  console.log(data);
+  //res.send(path);
+  //res.sendFile(__dirname+'/video.html')
+  /*const stat = fs.statSync(path)
+  const fileSize = stat.size
+  const range = req.headers.range
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1] 
+      ? parseInt(parts[1], 10)
+      : fileSize-1
+    const chunksize = (end-start)+1
+    const file = fs.createReadStream(path, {start, end})
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res)
+  }*/
+});
+
+
 
 app.post('/upload',  function(req,res){ 
 	upload(req,res,async function(err) {
@@ -47,12 +104,15 @@ app.post('/upload',  function(req,res){
     const caption= req.body.caption;
     console.log(caption);
 
-    const det=req.file["path"];
-    console.log(det);
+    //const det=req.file["path"];
+    //console.log(det);
     const path= req.file.path;
-    await save_file.create({path,caption});
-
-		res.end("File is uploaded successfully!");
+    console.log(path);
+    var keyword={'key':'ans','key':'ques'};
+    await save_file.create({path,caption,keyword});
+    
+    //alert("File is uploaded successfully!");
+		//res.end("File is uploaded successfully!");
     
 	});
 });
